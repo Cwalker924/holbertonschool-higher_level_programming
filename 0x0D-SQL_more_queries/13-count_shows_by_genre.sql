dd5fab42fef7e964ebf1d4a374a4083820f988c7
Cwalker924 <cwalker924@gmail.com>
2017-03-01 08:01:03 +0000

    fix for 13

--- 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 -- of shows linked to each.
 SELECT tv_genres.name AS 'genre',
 COUNT(tv_show_genres.genre_id) AS 'number_shows'
-FROM tv_genres JOIN tv_shows_genres
-WHERE tv_show_genres.genre_id = tv_genres.id
+FROM tv_genres INNER JOIN tv_shows_genres
+ON tv_genres.id = tv_shows_genres.genre_id
 GROUP BY tv_genres.name ORDER BY number_shows DESC;
