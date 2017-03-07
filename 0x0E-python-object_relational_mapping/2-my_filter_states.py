89d26b1b298e8e56ebc43ed1b924e44e34e83ee3
Cwalker924 <cwalker924@gmail.com>
2017-03-07 09:53:23 +0000

    fix for 2

--- 0x0E-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# a script that takes in an argument and displays all values in the states table
-# of hbtn_0e_0_usa where name matches the argument.
+# a script that takes in an argument and displays all values in the states
+# table of hbtn_0e_0_usa where name matches the argument.
 
 import sys
 import MySQLdb
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     usa_db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                              db=sys.argv[3], host="localhost", port=3306)
     cursor = usa_db.cursor()
-    cursor.execute("SELECT * From states ORDER BY id ASC;")
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC;"
+                   .format(sys.argv[4]))
     states = cursor.fetchall()
 
     for state in states:
