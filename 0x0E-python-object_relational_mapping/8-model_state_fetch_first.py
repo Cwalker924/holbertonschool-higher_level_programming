e3048861249e793cf2446ef749f9762706e81251
Cwalker924 <cwalker924@gmail.com>
2017-03-07 07:54:47 +0000

    fix for 8

--- 0x0E-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0E-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-    sesh = sessionmaker(bind=engine)
-    session = sesh()
+    sesh = sessionmaker()
+    session = sesh(bind=engine)
 
     query = session.query(State)
     trigger = 0
