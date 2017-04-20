ecea25766c0492d90bed698a1364085b6d711e17
Cwalker924 <cwalker924@gmail.com>
2017-04-20 08:15:24 +0000

    0-9

---
+++ 0x11-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+let firstLine = 'C is fun\n';
+let secondLine = 'Python is cool\n';
+let thirdLine = 'Javascript is amazing';
+console.log(firstLine + secondLine + thirdLine);
---
+++ 0x11-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+let myList = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i in myList) {
+  console.log(myList[i]);
+}
