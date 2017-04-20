fc5a3e4b8c02355c36e60158156eeadbd5fa2a6b
Cwalker924 <cwalker924@gmail.com>
2017-04-20 10:25:20 +0000

    fix for 11

--- 0x11-javascript-warm_up/11-second_biggest.js
+++ 0x11-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 2 || process.argv[2] === '1' ||
-    process.argv[2] === '0') {
+if (process.argv.length <= 2 || process.argv[2] === '1') {
   console.log(0);
 } else {
   let myList = [];
-  let i = 3;
+  let i = 2;
   while (i < process.argv.length) {
     myList.push(process.argv[i]);
     i++;
   }
   let a = myList.sort();
-  console.log(a[a.length - 1]);
+  console.log(a[a.length - 2]);
 }
