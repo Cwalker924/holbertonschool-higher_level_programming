99267bced2312dbbb2c705e797105104c33aa6da
Cwalker924 <cwalker924@gmail.com>
2017-04-20 10:45:24 +0000

    fix for 10

--- 0x11-javascript-warm_up/10-factorial.js
+++ 0x11-javascript-warm_up/10-factorial.js
@@ -2,16 +2,16 @@
 
 let num = parseInt(process.argv[2]);
 function fact (a) {
-  let i = 1;
   if (a === '0' || isNaN(a)) {
-    console.log(1);
+    return (1);
   } else {
+    let i = 1;
     while (num >= 1) {
       i *= num;
       num--;
     }
-    console.log(i);
+    return (i);
   }
 }
 
-fact(num);
+console.log(fact(num));
