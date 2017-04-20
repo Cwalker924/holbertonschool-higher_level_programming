5c673bf1840df409bb3a67ade0b81f1c62a6b893
Cwalker924 <cwalker924@gmail.com>
2017-04-20 09:48:12 +0000

    adding advanced and format fix

--- 0x11-javascript-warm_up/9-add.js
+++ 0x11-javascript-warm_up/9-add.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-a = parseInt(process.argv[2]);
-b = parseInt(process.argv[3]);
-function add(a, b) {
-    if (a && b) {
-	console.log(a + b);
-    } else {
-	console.log('NaN')
-    }
+let a = parseInt(process.argv[2]);
+let b = parseInt(process.argv[3]);
+function add (a, b) {
+  if (a && b) {
+    console.log(a + b);
+  } else {
+    console.log('NaN');
+  }
 }
-add(a, b)
+add(a, b);
