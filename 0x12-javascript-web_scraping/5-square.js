ae38641c58c7c58a6eac20ad32e477bafd2677ec
Cwalker924 <cwalker924@gmail.com>
2017-04-21 06:02:17 +0000

    fox for 5,6

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -3,6 +3,6 @@
 const Rectangle = require('./4-rectangle').Rectangle;
 
 exports.Square = function Square (size) {
-    Rectangle.call(this, size, size);
-    this.size = size;
+  Rectangle.call(this, size, size);
+  this.size = size;
 };
--- 0x12-javascript-web_scraping/6-square.js
+++ 0x12-javascript-web_scraping/6-square.js
@@ -7,9 +7,9 @@ Square.prototype.charPrint = function (c) {
     this.print();
   } else {
     let addC = '';
-      let x = 0;
+    let x = 0;
     while (x < this.size) {
-	addC = addC.concat(c);
+      addC = addC.concat(c);
       x++;
     }
     let y = 0;
@@ -17,9 +17,8 @@ Square.prototype.charPrint = function (c) {
       console.log(addC);
       y++;
     }
-      console.log(addC);
+    console.log(addC);
   }
 };
 
 module.exports = {Square};
-//module.Square = Square;
